0a6f3edd168e47b1f9a1d5e915db980dc3ccf80f
Jonathan Hong <jonhong777@gmail.com>
2018-08-20 03:59:01 +0000

    automatically backed up by learn

--- db/migrate/20180820035156_create_recipes.rb
+++ db/migrate/20180820035156_create_recipes.rb
@@ -3,7 +3,7 @@ class CreateRecipes < ActiveRecord::Migration
     create_table :recipes do |t|
       t.string :name
       t.string :ingredients
-      t.
+      t.datetime :cook_time
     end
   end
 end
