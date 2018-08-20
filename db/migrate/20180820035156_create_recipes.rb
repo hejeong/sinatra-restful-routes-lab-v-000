39fff1d523a03290d0075fdc58a1fb46a5df81f1
Jonathan Hong <jonhong777@gmail.com>
2018-08-20 03:58:19 +0000

    automatically backed up by learn

--- db/migrate/20180820035156_create_recipes.rb
+++ db/migrate/20180820035156_create_recipes.rb
@@ -1,7 +1,9 @@
 class CreateRecipes < ActiveRecord::Migration
   def change
-    create_table :recipes do |t|
+    create_table:recipes do |t|
       t.string :name
+      t.string :ingredients
+      t.
     end
   end
 end
